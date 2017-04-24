59cecf63629c4626809739c37ec29d1dba1f221e
Matthew James Harrison <mjsonofharry@gmail.com>
2017-04-24 16:28:20 -0400

    Created header for Vector class

---
+++ vector.hpp
@@ -0,0 +1,27 @@
+#include "fvmath.hpp"
+
+
+class Vector
+{
+	private:
+	__m128 mData;
+
+	public:
+	Vector();
+	~Vector();
+
+	float x();
+	float y();
+	float z();
+	float w();
+	float len();
+	Vector unit();
+	float dotproduct(const Vector &op);
+	Vector projection(const Vector &op);
+
+	Vector operator=(const Vector &op);
+	Vector operator+(const Vector &op);
+	Vector operator-(const Vector &op);
+	Vector operator*(const float &op);
+	Vector operator/(const float &op);
+}
