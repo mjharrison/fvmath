c3a491245cf4466f9724e74506df2b7ec2a581a0
Matthew James Harrison <mjsonofharry@gmail.com>
2017-04-24 14:57:55 -0400

    Added header declarations for "_"-prefixed functions

--- fvmath.hpp
+++ fvmath.hpp
@@ -12,12 +12,16 @@ void fvadd(float vectorA[4], float vectorB[4]);
 
 void fvsub(float vectorA[4], float vectorB[4]);
 
+__m128 _fvlen(__m128 a);
 float fvlen(float vector[4]);
 
+__m128 _fvnorm(__m128 a);
 void fvnorm(float vector[4]);
 
+__m128 _fvdotp(__m128 a, __m128 b);
 float fvdotp(float vectorA[4], float vectorB[4]);
 
+__m128 _fvproj(__m128 a, __m128 b);
 void fvproj(float vectorA[4], float vectorB[4]);
 
 
