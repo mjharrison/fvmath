d6ce6829cd70b060f324a65f9f3dc1e12336a0e5
Matthew James Harrison <mjsonofharry@gmail.com>
2017-04-27 13:11:26 -0400

    Fixed header guards and finished definitions

--- vector.cpp
+++ vector.cpp
@@ -30,7 +30,17 @@ float Vector::w()
 
 float Vector:len()
 {
-	return _fvlen(mData);
+	float out;
+	__m128 vect = _mm_mul_ps(mData, mData);
+
+	__m128 shuf = _mm_movehdup_ps(vect);
+	__m128 sum = _mm_add_ps(vect, shuf);
+	shuf = _mm_movehl_ps(shuf, sum);
+	sum = _mm_add_ss(sum, shuf);
+
+	vect = _mm_sqrt_ps(sum);
+
+	return _mm_store_ss(&out, len);
 }
 
 
@@ -42,13 +52,13 @@ Vector Vector::unit()
 
 float Vector::dotproduct(const Vector &op)
 {
-	return _fvdotp(mData, op);
+	return _fvdotp(mData, op.mData);
 }
 
 
 Vector Vector::projection(const Vector &op)
 {
-	return _fvproj(mData, op);
+	return Vector(_fvproj(mData, op.mData));
 }
 
 
@@ -60,10 +70,28 @@ void Vector::operator=(const Vector &op)
 
 Vector Vector::operator+(const Vector &op)
 {
-	
+	return Vector(_mm_add_ps(mData, op.mData));
 }
 
 
-Vector Vector::operator-(const Vector &op);
-Vector Vector::operator*(const float &op);
-Vector Vector::operator/(const float &op);
+Vector Vector::operator-(const Vector &op)
+{
+	return Vector(_mm_sub_ps(mData, op.mData));
+}
+
+
+Vector Vector::operator*(const float &op)
+{
+	return Vector(_mm_mul_ps(mData, op.mData));
+}
+
+
+Vector Vector::operator/(const float &op)
+{
+	return Vector(_mm_div_ps());
+}
+
+Vector Vector::operator*(const Vector &op)
+{
+	return _fvdotp(mData, op.mData);
+}
